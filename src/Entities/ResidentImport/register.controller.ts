a554cd87999e899cccef7ef8996278bb76c9503b
Rafael Sequeiros <rafanetow@gmail.com>
2020-07-18 18:52:00 -0600

    delete typos

--- src/Entities/ResidentImport/register.controller.ts
+++ src/Entities/ResidentImport/register.controller.ts
@@ -1,35 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateRegisterImportDto } from './dto/create-registerImport.dto';
 import { RegisterImport } from './registerImport.entity';
-import { Regoste } from './registerImport.service';
+import { RegisterImportService } from './registerImport.service';
 import { } from './registerImport.entity'
+import { DeleteResult } from 'typeorm';
 
 
 
 @Controller('registerImport')
 export class CarController {
-    constructor(private readonly carService: CarService) { }
+    constructor(private readonly registerService: RegisterImportService) { }
 
     @Post()
     create(@Body() createCartDto: CreateRegisterImportDto): Promise<RegisterImport> {
-        return this.carService.save(createCartDto);
+        return this.registerService.save(createCartDto);
     }
 
 
     @Get()
     findAll(): Promise<Car[]> {
-        return this.carService.findAll();
+        return this.registerService.findAll();
     }
 
     @Get(':placa')
     findOne(@Param('placa') placa: string): Promise<RegisterImport> {
-        return this.carService.findByPlaca(placa);
+        return this.registerService.findByPlaca(placa);
     }
 
 
 
     @Delete(':id')
-    remove(@Param('id') id: string): Promise<void> {
-        return this.carService.remove(RegisterImport);
+    remove(@Param('id') id: string): Promise<DeleteResult> {
+        return this.registerService.remove(id);
     }
 }
